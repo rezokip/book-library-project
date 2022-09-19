0d70f806d25129db21d061dcd856c7ceff5c8359
ozerkip <ozerkip@hotmail.de>
2022-09-19 18:30:05 +0200

    added more js and js comments

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // select the html elements 
 
 let myLibraryEl = document.querySelector('.library-section-container')
+let testButton = document.querySelector('.test-button')
+
+let titleInput = document.querySelector('#title')
+let authorInput = document.querySelector('#author')
+let pagesInput = document.querySelector('#pages')
+let readInput = document.querySelector('#read')
+
+
+
+// event listener of the add button, to create a new book with the input values
+  // after creating the new book add it to the library and add it to the dom
+testButton.addEventListener('click', function(){
+  let title = titleInput.value
+  let author = authorInput.value
+  let pages = pagesInput.value
+  let read = readInput.value
+  console.log(title)
+  console.log(author)
+  console.log(pages)
+  console.log(read)
+
+  let newBook = new Book (title,author,pages,read)
+  console.log(newBook)
+  addBookToLibrary(newBook)
+  addToDom()
+
+})
 
 
 
@@ -39,30 +66,60 @@ addBookToLibrary(book5)
 
 
 // function for adding the books from the myLibrary array to the DOM
+  // begin the library el without textcontent everytime you add something to the DOM
   // loop through every book in the myLabrary array
     // create the appropriate elements such as title, author...
     // give them the right textcontent
     // append them to the parent container to show them on the page 
 function addToDom(){
+  myLibraryEl.textContent=""
   for(book of myLibrary){
-    console.log(book.title)
-    console.log(book)
     
     
+    bookIndex=myLibrary.indexOf(book)
+    
+    
+    
+    //create the Elements
+    bookContainer = document.createElement('div')
+    bookDeleteButton = document.createElement('button')
     bookUlEl = document.createElement('ul')
     bookTitleEl = document.createElement('li')
     bookAuthorEl = document.createElement('li')
     bookPagesEl = document.createElement('li')
     bookReadEl = document.createElement('li')
-    
+
+ 
+    // give them textcontent
+    bookDeleteButton.textContent = 'x'
     bookTitleEl.textContent = `Title: ${book.title}`
     bookAuthorEl.textContent =  `Author: ${book.author}`
     bookPagesEl.textContent =  `Pages: ${book.pages}`
     bookReadEl.textContent = book.read
-
+   
+    //append them to the parents
     bookUlEl.append(bookTitleEl, bookAuthorEl, bookPagesEl, bookReadEl)
-    myLibraryEl.appendChild(bookUlEl)
+    bookContainer.append( bookDeleteButton, bookUlEl)
+    myLibraryEl.appendChild(bookContainer)
+
+    // give delete button a class with its own index in the myLibrary array
+    buttonClass=bookIndex
+    bookDeleteButton.classList.add(buttonClass)
+    // when clicking the button, delete its parent with parentNode method
+    bookDeleteButton.addEventListener('click', function(){
+      console.log(this.parentNode)
+      
+      console.log(this.classList.value)
+
+      this.parentNode.remove()
+      myLibrary.splice(buttonClass,1)
+      addToDom()
+      
+    })
   }
 }
 
 addToDom()
+
+
+
